d7363d436841620303b2bf9821aefd08d297f5ee
Luana Cardiga Bianchi <luana_cardiga@hotmail.com>
2020-05-31 11:45:42 -0300

    Add files via upload

---
+++ OlaMundo.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// Name        : OlaMundo.cpp
+// Author      : Luana Cardiga Bianchi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+//Exibe no terminal a mensagem"olá, mundo"
+
+/*
+ * comentário de várias linhas
+ */
+
+/**
+ * comentário para ajudar a documentar o código
+ * Função principal
+ * @return estado final do programa
+ */
+
+float soma (float a, float b){
+	return a + b;
+}
+
+int main() {
+	cout << "Olá, mundo!" << endl; // prints
+	cout << soma(10, 5.6) << endl;
+	return 0;
+}
+
